2f35ab619468da15262a0e8253f9cbb37aae550c
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-12-30 15:45:29 -0700

    update graph variant, update bun lock file
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- docs/src/pages/graphql.tsx
+++ docs/src/pages/graphql.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import Layout from '@theme/Layout';
 
 export default function GrapqhlExplorer() {
-    return (
-        <Layout title="Graphql" description="Openlane Graphql Explorer">
-            <ApolloExplorer
-                className='graphql'
-                graphRef='Openlane-2fwyqq@12-12-2024'
-                persistExplorerState={false}
-                initialState={{
-                    document: `query Programs {
+  return (
+    <Layout title="Graphql" description="Openlane Graphql Explorer">
+      <ApolloExplorer
+        className='graphql'
+        graphRef='Openlane-2fwyqq@current'
+        persistExplorerState={false}
+        initialState={{
+          document: `query Programs {
   programs {
     edges {
       node {
@@ -23,19 +23,19 @@ export default function GrapqhlExplorer() {
   }
 }
 `,
-                    variables: {},
-                    headers: {
-                        'Authorization': 'Bearer YOUR_TOKEN',
-                    },
-                    displayOptions: {
-                        showHeadersAndEnvVars: true,
-                        showGlobalHeader: false,
-                        docsPanelState: 'open',
-                        theme: 'dark',
-                    },
-                }}
+          variables: {},
+          headers: {
+            'Authorization': 'Bearer YOUR_TOKEN',
+          },
+          displayOptions: {
+            showHeadersAndEnvVars: true,
+            showGlobalHeader: false,
+            docsPanelState: 'open',
+            theme: 'dark',
+          },
+        }}
 
-            />
-        </Layout >
-    );
+      />
+    </Layout >
+  );
 }
